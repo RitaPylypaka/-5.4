8fbc483801164c87ecfcae6f483c26218b903331
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-25 19:30:15 +0300

    Add project files.

---
+++ UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр5.4/пр5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double v = S0(2, 5);
+			Assert::AreEqual(v, -0.298258, 0.000001);
+
+		}
+	};
+}
